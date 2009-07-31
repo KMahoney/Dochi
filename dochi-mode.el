32b4d9b2e33684183aa9b4172bf285747057b9ae
Kevin Mahoney <kevin@kevinmahoney.co.uk>
2009-07-31 14:21:01 +0100

    emacs mode changes

--- dochi-mode.el
+++ dochi-mode.el
@@ -5,9 +5,9 @@
 (define-generic-mode dochi-mode 
   '(?#)
   '("def" "module" "use" "t" "f")
-  '((":[[:alnum:]-_&*?<>]+" . font-lock-type-face)
-    ("def\\s-+\\(\\S-+\\)" 1 font-lock-function-name-face))
+  '(("\\B:[-[:alnum:]_&*?<>]+" . font-lock-constant-face)
+    ("def\\s-+\\(\\S-+\\)" 1 font-lock-function-name-face)
+    ("(\\([^)]+\\))" 1 font-lock-variable-name-face)
+    ("\\(L\\|T\\|C\\|match\\|sig\\){" 1 font-lock-keyword-face))
   '("\.chi$")
   '(dochi-syntax))
-
-
